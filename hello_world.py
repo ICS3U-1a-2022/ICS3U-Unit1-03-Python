69a90b48a9e91bd7e3fd47e9ae041cf5755422f0
ICS3U.1 2022 <ics3u.1.2022@mths.ca>
2022-09-15 14:37:01 +0000

    init

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Mr. Coxall
+# Created on: Sep 2022
+# This is the "Hello, World!" program, but with nice style
+
+def main():
+    # this function prints to the console
+
+    print("Hello, World!")
+
+    print("\nDone.")
+
+if __name__ == "__main__":
+    main()
