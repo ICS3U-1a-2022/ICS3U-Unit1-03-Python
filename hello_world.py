b1e75312871c680995c1d714c2b1a688fa1930d1
ICS3U.1 2022 <ics3u.1.2022@mths.ca>
2022-09-15 14:44:22 +0000

    fix linting errors

--- hello_world.py
+++ hello_world.py
@@ -4,12 +4,14 @@
 # Created on: Sep 2022
 # This is the "Hello, World!" program, but with nice style
 
+
 def main():
     # this function prints to the console
 
     print("Hello, World!")
 
-    print("\nDone. ")
+    print("\nDone.")
+
 
 if __name__ == "__main__":
     main()
